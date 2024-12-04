1b5d949ed1a13dc6ffbc2c7d7f3d9d919a09d466
HRC10 <hugo.castor15@gmail.com>
2024-12-04 15:19:17 -0300

    resolving 1196

---
+++ 1196.js
@@ -0,0 +1,22 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+let testes = parseInt(lines.shift());
+let i = 0;
+let casas = 0;
+let graos = 1;
+let j = 1;
+let resultado = 0;
+while (i < testes){
+    casas = parseInt(lines.shift());
+    graos = 1;
+    j = 1;
+    while (j <= casas){
+        graos = graos * 2;
+        j++;
+    }
+    graos = ((graos / 12) / 1000)
+console.log(Math.trunc(graos),"kg")
+    i++;
+}
+ 
